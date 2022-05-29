d74b88607db0da6227bcc32da4f6c98fd499ac71
cbag1 <bacargoudiaby@gmail.com>
2022-05-29 22:15:27 +0000

    Ajout Eleves

--- src/app/eleves/eleves.component.ts
+++ src/app/eleves/eleves.component.ts
@@ -8,13 +8,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ElevesComponent implements OnInit {
 
+  eleveNom = "";
+  sexe = "";
   classeId: number = 0;
   eleves = [
     { id: 1, nom: "Cheikh Babacar GOUDIABY", sexe: "M", idClasse: 1 },
     { id: 2, nom: "Cheikh GOUDIABY", sexe: "M", idClasse: 1 },
     { id: 3, nom: "Kara GOUDIABY", sexe: "M", idClasse: 2 }
 
-    
+
   ];
 
   elevesfiltre = this.eleves;
@@ -23,12 +25,26 @@ export class ElevesComponent implements OnInit {
   }
   ngOnInit(): void {
 
-    this.classeId = this.route.snapshot.params['id'];
+    this.classeId = Number(this.route.snapshot.params['id']);
     console.log(this.classeId);
-    this.elevesfiltre = this.eleves.filter((eleve)=>this.classeId == eleve.idClasse);
-
+    this.elevesfiltre = this.chargerEleves();
   }
 
-  
+
+  addEleve() {
+    const lastIndex = this.eleves.length - 1;
+    const id = this.eleves[lastIndex].id + 1;
+    this.eleves.push({ id, nom: this.eleveNom, sexe: this.sexe, idClasse: this.classeId });
+    this.eleveNom = "";
+    this.sexe = "";
+    // console.log(this.eleves);
+    this.elevesfiltre= this.chargerEleves();
+  }
+
+  chargerEleves(){
+    return this.eleves.filter((eleve) => this.classeId === eleve.idClasse);
+    
+  }
+
 
 }
