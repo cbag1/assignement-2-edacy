1460a5d28e100a02ba6a5432d0904e32a8933311
cbag1 <bacargoudiaby@gmail.com>
2022-05-29 21:10:11 +0000

    Assignement-2-edacy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignement-2-edacy';
+
+  classeNom = "";
+
+
+  classes = [
+    {
+      id: 1, nom: "Terminal S2"
+    }
+  ]
+
+
+
+  addClasse() {
+    const lastIndex = this.classes.length - 1;
+    const id = this.classes[lastIndex].id + 1;
+    this.classes.push({ id, nom: this.classeNom });
+    this.classeNom = "";
+  }
 }
